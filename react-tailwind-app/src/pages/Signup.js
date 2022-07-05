db3585bb51064565fcfb25b7e031564ff18ccc28
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-05 16:15:24 +0300

    imported the signup component

--- react-tailwind-app/src/pages/Signup.js
+++ react-tailwind-app/src/pages/Signup.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Header from "../components/Header";
+import Signup from "../components/Signup";
 
-export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+export default function SignupPage(){
+    return(
+        <>
+            <Header
+              heading="Signup to create an account"
+              paragraph="Already have an account? "
+              linkName="Login"
+              linkUrl="/"
+            />
+            <Signup/>
+        </>
+    )
 }
